048e0b4ba2beb654aa753b67b7ff90ea545d6dc5
PRASANTHBHARADHWAAJ <prasanth99waaj@gmail.com>
2020-03-05 09:19:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-march5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-march5)
---
+++ index.js
@@ -0,0 +1,14 @@
+//FIBONACCI SERIES
+
+let a = 1
+let b = 1
+console.log(a)
+console.log(b)
+// let n = prompt("Enter value");
+for(i=0;i<n;i++)
+{
+  a=a+b;
+  b=a-b;
+  console.log(a);
+}
+
